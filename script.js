1098bd5d79daf589d0a19454b7f11f8215296bc1
Erden <zerdendereli@icloud.com>
2025-03-26 20:43:52 +0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-window.onload = function () {
+document.querySelector("button").addEventListener("click", function () {
     
     var firstDice = Math.floor(Math.random() * 6) + 1;
+     
     var img1 = document.getElementsByClassName("img1")[0];
     var randomSource1 = "images/dice" + firstDice + ".png";
     img1.setAttribute("src", randomSource1);
@@ -17,10 +18,12 @@ window.onload = function () {
 
 
     if (firstDice > secondDice) {
-        document.querySelector("h1").innerHTML = "<h1>🎉 Player 1 wins!<h1>";
+        document.querySelector("h1").innerHTML = "<h1>🎉 Player 1 wins!</h1>";
     } else if (firstDice < secondDice) {
-        document.querySelector("h1").innerHTML = "<h1>🎉 Player 2 wins!<h1>";
+        document.querySelector("h1").innerHTML = "<h1>🎉 Player 2 wins!</h1>";
     } else {
-        document.querySelector("h1").innerHTML = "<h1>&#128531; It's a draw !<h1>";
+        document.querySelector("h1").innerHTML = "<h1>&#128531; It's a draw !</h1>";
     }
-}
+
+    document.querySelector("h6").textContent = "Press to play again!";
+});
